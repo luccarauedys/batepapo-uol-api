729107c8abf4a7e8a1b0e65405ec5e5ec86b5385
Lucca Rauédys <luccarauedys@outlook.com>
2022-05-01 01:00:51 -0300

    requisicoes get e post de mensagens e ajustes nas demais rotas

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import cors from "cors"
 import dayjs from "dayjs"
 import dotenv from "dotenv"
 
-import { validateParticipant } from "./utils/validationSchemas.js"
+import {
+  validateParticipant,
+  validateMessage,
+} from "./utils/validationSchemas.js"
 
 dotenv.config()
 
 const app = express().use(cors()).use(json())
 
+let username = null
 let database = null
 const mongoClient = new MongoClient(process.env.MONGO_URI)
 mongoClient
@@ -22,8 +26,13 @@ mongoClient
 
 app.get("/participants", async (req, res) => {
   const participants = database.collection("participants")
-  const allParticipants = await participants.find().toArray()
-  res.send(allParticipants)
+  try {
+    const allParticipants = await participants.find().toArray()
+    res.send(allParticipants)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 app.post("/participants", async (req, res) => {
@@ -33,26 +42,82 @@ app.post("/participants", async (req, res) => {
 
   const validation = validateParticipant(participant)
   if (validation.error) {
+    const errors = validation.error.details.map((detail) => detail.message)
+    console.log(errors)
     return res.sendStatus(422)
   }
+  username = req.body
 
-  const alreadyExists = await participants.find(participant).toArray()
-  if (alreadyExists.length > 0) {
-    return res.sendStatus(409)
+  try {
+    const alreadyExists = await participants.find(participant).toArray()
+    if (alreadyExists.length > 0) {
+      return res.status(409).send("this user already exists")
+    }
+
+    await participants.insertOne({ ...participant, lastStatus: Date.now() })
+    const message = {
+      from: participant.name,
+      to: "Todos",
+      text: "entra na sala...",
+      type: "status",
+      time: dayjs().format("HH:mm:ss"),
+    }
+    await messages.insertOne(message)
+    res.sendStatus(201)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
   }
+})
 
-  await participants.insertOne({ ...participant, lastStatus: Date.now() })
+app.post("/messages", async (req, res) => {
+  const message = req.body
+  const { user } = req.headers
+  const participants = database.collection("participants")
+  const messages = database.collection("messages")
 
-  const message = {
-    from: participant.name,
-    to: "Todos",
-    text: "entra na sala...",
-    type: "status",
-    time: dayjs().format("HH:mm:ss"),
+  try {
+    const messageValidation = validateMessage(message)
+    const userValidation = await participants.find({ name: user }).toArray()
+    if (messageValidation.error) {
+      const errors = messageValidation.error.details.map(
+        (detail) => detail.message
+      )
+      console.log(errors)
+      return res.sendStatus(422)
+    }
+    if (userValidation.length === 0) {
+      return res.status(422).send("this user is not in the chat room")
+    }
+
+    await messages.insertOne({
+      ...message,
+      from: user,
+      time: dayjs().format("HH:mm:ss"),
+    })
+    res.sendStatus(201)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
   }
-  await messages.insertOne(message)
+})
 
-  res.sendStatus(201)
+app.get("/messages", async (req, res) => {
+  const limit = parseInt(req.query.limit) || null
+  const messages = database.collection("messages")
+  try {
+    const allMessages = await messages.find().toArray()
+    const allowedMessages = allMessages.filter((message) => {
+      return message.to === "Todos" || message.to === username
+    })
+    if (limit) {
+      return res.send(allowedMessages.slice(`-${limit}`))
+    }
+    res.send(allowedMessages)
+  } catch (err) {
+    console.log(err)
+    res.sendStatus(500)
+  }
 })
 
 app.listen(process.env.PORT, () => {
